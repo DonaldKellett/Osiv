71c935c85b3edfdd022f2dc5e13d942d0d01c8a7
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-03-26 22:24:16 +0800

    Add MySQL script for creating account database

---
+++ osiv.sql
@@ -0,0 +1,11 @@
+CREATE USER 'osiv'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+CREATE DATABASE osiv;
+GRANT ALL PRIVILEGES ON osiv.* TO 'osiv'@'localhost';
+USE osiv;
+CREATE TABLE Accounts (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(32),
+  password BINARY(60),
+  prettyName VARCHAR(1024),
+  privileged BOOL
+);
