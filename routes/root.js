c95622ba28ee960e8211280ea5b54c3f6539d718
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-03-26 20:07:10 +0800

    Implement GET / route

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,10 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    return {
+      name: 'Osiv',
+      version: '0.1.0',
+      timeout: 86400
+    }
   })
 }
