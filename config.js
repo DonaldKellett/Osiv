1f7cfc5eb4f94ae52b9c28008b7a8240eca7de36
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-03-27 14:50:27 +0800

    Collapse creds.js into config.js

--- config.js
+++ config.js
@@ -1,6 +1,25 @@
 'use strict'
 
-module.exports = {
-  validUsername: /^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$/,
-  saltRounds: 10
+const path = require('path')
+const fs = require('fs')
+
+const OSIV_CONF_BASE = process.env.OSIV_CONF_BASE || __dirname
+try {
+  const DB_PW = fs.readFileSync(path.join(OSIV_CONF_BASE, 'db-pw'))
+    .toString()
+    .slice(0, -1)
+  const MASTER_PW = fs.readFileSync(path.join(OSIV_CONF_BASE, 'master-pw'))
+    .toString()
+    .slice(0, -1)
+  const VALID_USERNAME = /^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$/
+  const SALT_ROUNDS = 10
+  module.exports = {
+    DB_PW,
+    MASTER_PW,
+    VALID_USERNAME,
+    SALT_ROUNDS
+  }
+} catch (err) {
+  console.error(err)
+  process.exit(1)
 }
--- routes/signup/index.js
+++ routes/signup/index.js
@@ -5,11 +5,8 @@ const mysql = require('mysql-await')
 // Temporary workaround for broken connection.escape() in mysql-await
 const { escape } = require('mysql')
 const bcrypt = require('bcrypt')
-const creds = require('../../creds')
 const config = require('../../config')
 
-const DB_PW = creds.dbPw
-const MASTER_PW = creds.masterPw
 const bodyJsonSchema = S.object()
   .prop('privileged', S.boolean()).required()
   .prop('prettyName', S.string()).required()
@@ -18,31 +15,29 @@ const bodyJsonSchema = S.object()
   .prop('masterPassword', S.string())
 const schema = { body: bodyJsonSchema }
 // Credits: https://unix.stackexchange.com/a/435120
-const VALID_USERNAME = config.validUsername
-const SALT_ROUNDS = config.saltRounds
 
 module.exports = async function (fastify, opts) {
   fastify.post('/', { schema }, async function (request, reply) {
     const { privileged, prettyName, username, password, masterPassword } = request.body
     if (prettyName.length > 1024)
       return reply.status(400).send({ message: 'The provided prettyName should not exceed 1024 characters' })
-    if (!VALID_USERNAME.test(username))
+    if (!config.VALID_USERNAME.test(username))
       return reply.status(400).send({ message: 'The provided username should be a valid Linux username' })
     if (password.length < 8 || password.length > 50)
       return reply.status(400).send({ message: 'The provided password should be between 8 and 50 characters long' })
-    if (privileged && masterPassword !== MASTER_PW)
+    if (privileged && masterPassword !== config.MASTER_PW)
       return reply.status(401).send({ message: 'Attempted to create privileged account with incorrect or missing master password' })
     try {
       let connection = mysql.createConnection({
         host: 'localhost',
         user: 'osiv',
-        password: DB_PW,
+        password: config.DB_PW,
         database: 'osiv'
       })
       let users = await connection.awaitQuery(`SELECT * FROM Accounts WHERE username = ${escape(username)}`)
       if (users.length > 0)
         return reply.status(400).send({ message: 'An account with the given username already exists' })
-      let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+      let hashedPassword = await bcrypt.hash(password, config.SALT_ROUNDS)
       await connection.awaitQuery(`INSERT INTO Accounts (username, password, prettyName, privileged) VALUES (${escape(username)}, ${escape(hashedPassword)}, ${escape(prettyName)}, ${privileged ? 'TRUE' : 'FALSE'})`)
       await connection.awaitEnd()
       return reply.status(201).send({})
