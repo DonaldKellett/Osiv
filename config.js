2bcba883918c33dc1837d20ef12af1823a3c627e
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-03-27 19:23:42 +0800

    Implement POST /login endpoint

--- config.js
+++ config.js
@@ -5,17 +5,27 @@ const fs = require('fs')
 
 const OSIV_CONF_BASE = process.env.OSIV_CONF_BASE || __dirname
 try {
+  const NAME = 'Osiv'
+  const VERSION = '0.1.0'
+  const TIMEOUT = 86400
   const DB_PW = fs.readFileSync(path.join(OSIV_CONF_BASE, 'db-pw'))
     .toString()
     .slice(0, -1)
   const MASTER_PW = fs.readFileSync(path.join(OSIV_CONF_BASE, 'master-pw'))
     .toString()
     .slice(0, -1)
+  const JWT_SECRET = fs.readFileSync(path.join(OSIV_CONF_BASE, 'jwt-secret'))
+    .toString()
+    .slice(0, -1)
   const VALID_USERNAME = /^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$/
   const SALT_ROUNDS = 10
   module.exports = {
+    NAME,
+    VERSION,
+    TIMEOUT,
     DB_PW,
     MASTER_PW,
+    JWT_SECRET,
     VALID_USERNAME,
     SALT_ROUNDS
   }
--- routes/root.js
+++ routes/root.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const config = require('../config')
+
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return {
-      name: 'Osiv',
-      version: '0.1.0',
-      timeout: 86400
+      name: config.NAME,
+      version: config.VERSION,
+      timeout: config.TIMEOUT
     }
   })
 }
